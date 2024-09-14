8925ad5085bce45db15397eb100b16d7c6103bce
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-09-14 12:17:47 -0500

    .

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-import mysql from "mysql2/promise";
 import {
   DB_HOST,
   DB_USER,
@@ -6,6 +5,8 @@ import {
   DB_PORT,
   DB_DATABASE,
 } from "./config.js";
+
+import mysql from "mysql2/promise";
 
 const config = {
   host: DB_HOST,
@@ -16,4 +17,20 @@ const config = {
   // connectionLimit: 10 -> Si no se especifica, por defecto será 10
 };
 
-export const pool = mysql.createPool(config);
+const pool = mysql.createPool(config);
+
+// Verificar la Conexión a la BD
+const verifyConnection = async () => {
+  try {
+    const connection = await pool.getConnection(); // Obtener una conexión
+    console.log("Conexión exitosa a la Base de Datos.");
+    connection.release(); // Liberar la conexión y devolverla al pool
+  } catch (error) {
+    console.error("Error en la conexión a la Base de Datos", error.stack);
+  }
+};
+
+// Ejecutamos la verificación
+verifyConnection();
+
+export { pool };
